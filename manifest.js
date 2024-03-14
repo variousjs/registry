fcbe59cc8ec1e1fda27ad7347d317a307cab2db5
loeify <loeify@gmail.com>
2024-03-14 16:13:20 +0800

    fix: types

--- manifest.js
+++ manifest.js
@@ -3,6 +3,20 @@ const { readdirSync, existsSync, readFileSync, writeFileSync } = require('fs')
 const { load } = require('js-yaml')
 const { version } = require('./package.json')
 
+const template = `interface Package {
+  name: string,
+  description: string,
+  homepage: string,
+  'dist-tags': Record<'latest', string>,
+  versions: Record<string, {
+    dist: string,
+    dependencies?: Record<string, string>,
+  }>,
+}
+
+export type Packages = Record<packageNames, Package>
+`
+
 const manifests = readdirSync(resolve(__dirname, './packages'))
   .map((dir) => resolve(__dirname, './packages', dir, 'manifest.yml'))
   .filter((path) => existsSync(path))
@@ -19,4 +33,7 @@ const manifests = readdirSync(resolve(__dirname, './packages'))
   })
   .reduce((prev, cur) => ({ ...prev, [cur.name]: cur }), {})
 
+const packageNames = Object.keys(manifests).map((s) => `'${s}'`).join(' | ')
+
 writeFileSync(resolve(__dirname, './', 'registry.json'), JSON.stringify(manifests))
+writeFileSync(resolve(__dirname, './', 'index.d.ts'), template.replace('packageNames', packageNames))
